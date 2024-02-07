b0ce51cf3f5bc56a10c893233b1f5d9cb7ddef60
Tyler Gorski <115673766+tgorsk15@users.noreply.github.com>
2024-02-06 18:18:36 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+console.log("Why are we still here?");
+console.log("Yes");
+
+function templateStarter() {
+  console.log("is Prettier working?");
+}
+
+function newList() {
+  console.log("yay");
+}
